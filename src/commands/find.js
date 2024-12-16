c81c3562bf2e55328ea0d959d332612e67c5f0e7
Vikas Potluri <vikaspotluri123.github@gmail.com>
2024-12-16 14:22:29 -0800

    implement commands.find#update

--- src/commands/find.js
+++ src/commands/find.js
@@ -1,18 +1,127 @@
 // @ts-check
 const {exit} = require('node:process');
+const fs = require('node:fs/promises');
 
 /**
  * @typedef {import('../types').ParsedTheme} ParsedTheme
  * @typedef {'update' | 'fail' | 'json' | 'verbose'} Flag
  * @typedef {never} Parameter
  * @typedef {Record<Flag, boolean> & Record<Parameter, string>} Options
+ *
+ * @typedef {{
+  text: string;
+  location: import('../types.js').SourceLocation;
+ }} UpdateItem
+ * @typedef {Record<string, Array<UpdateItem>>} ChangesPerFile
  */
+
+/**
+ * @param {ParsedTheme['visitor']['textToTranslate']} textToTranslate
+ */
+function prepareTranslationTextForUpdate(textToTranslate) {
+  /**
+   * @type {ChangesPerFile}
+   */
+  const changesPerFile = {};
+
+  for (const [token, sources] of textToTranslate.entries()) {
+    for (const location of sources) {
+      const store = changesPerFile[location.source] ??= [];
+      store.push({text: token, location});
+    }
+  }
+
+  for (const store of Object.values(changesPerFile)) {
+    // Sort by line number decrementing so we don't have to compute offsets as we insert text
+    store.sort((a, b) => {
+      const lineDifference = b.location.start.line - a.location.start.line;
+      if (lineDifference !== 0) {
+        return lineDifference;
+      }
+
+      return b.location.start.column - a.location.start.column;
+    });
+  }
+
+  return changesPerFile;
+}
+
+/**
+ * @param {string} text
+ */
+function wrapTextInTranslationHelper(text) {
+  let quote = '"';
+
+  if (text.includes('"')) {
+    if (text.includes('\'')) {
+      text = text.replaceAll('"', '\\"');
+    } else {
+      quote = '\'';
+    }
+  }
+
+  return `{{t ${quote}${text}${quote}}}`;
+}
+
+/**
+ * @param {ParsedTheme} theme
+ * @param {ChangesPerFile} changesPerFile
+ * @param {boolean} verbose
+ */
+async function applyThemeChanges(theme, changesPerFile, verbose) {
+  const promises = [];
+  for (const file of theme.files) {
+    if (!Object.hasOwn(changesPerFile, file.path)) {
+      continue;
+    }
+
+    const filePath = `${theme.themePath}/${file.path}`;
+    const changes = changesPerFile[file.path];
+    const lines = file.contents.split('\n');
+    const messages = [];
+
+    if (!verbose) {
+      messages.push(`Updating ${filePath}`);
+    }
+
+    for (const {location, text} of changes) {
+      if (location.start.line !== location.end.line) {
+        throw new Error('Cannot update multiline strings');
+      }
+
+      const lineIndex = location.start.line - 1;
+      const updatedText = wrapTextInTranslationHelper(text);
+
+      if (verbose) {
+        // Don't include the column number because it won't be correct when there are multiple changes on one line
+        messages.push(`${filePath}:${location.start.line} ${text} -> ${updatedText}`);
+      }
+
+      const line = lines[lineIndex];
+      const prefix = line.slice(0, location.start.column);
+      const suffix = line.slice(location.end.column);
+      lines[lineIndex] = `${prefix}${updatedText}${suffix}`;
+    }
+
+    // Since we make the changes bottom-up, reverse the messages so the user seems them top-down
+    messages.reverse();
+    for (const message of messages) {
+      console.log(message);
+    }
+
+    promises.push(
+      fs.writeFile(filePath, lines.join('\n'))
+    );
+  }
+
+  return Promise.all(promises);
+}
 
 /**
  * @param {Options} options
  * @param {ParsedTheme} theme
  */
-function findCommand (options, theme) {
+function findCommand(options, theme) {
   const {update, fail, json, verbose} = options;
 
   if (update && fail) {
@@ -21,7 +130,8 @@ function findCommand (options, theme) {
   }
 
   if (update) {
-    throw new Error('Not implemented');
+    const changes = prepareTranslationTextForUpdate(theme.visitor.textToTranslate);
+    return applyThemeChanges(theme, changes, verbose);
   }
 
   const {themePath} = theme;
