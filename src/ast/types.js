7246baaa7dbdbda035e0c81406c2b2b6ffefd4b7
Vikas Potluri <vikaspotluri123.github@gmail.com>
2024-12-16 14:22:29 -0800

    add translated strings visitor

--- src/ast/types.js
+++ src/ast/types.js
@@ -1,6 +1,11 @@
+// @ts-check
 /**
  * @typedef {import('handlebars').Visitor} Visitor
  * @typedef {Parameters<Visitor['accept']>[0]} Node
+ * @typedef {Parameters<Visitor['MustacheStatement']>[0]} MustacheStatement
+ * @typedef {Parameters<Visitor['SubExpression']>[0]} SubExpression
+ * @typedef {Parameters<Visitor['PathExpression']>[0]} PathExpression
+ * @typedef {Parameters<Visitor['StringLiteral']>[0]} StringLiteral
  * @typedef {Parameters<Visitor['Program']>[0]} Program
  * @typedef {Program['loc']} SourceLocation
  * @typedef {SourceLocation['start']} Position
---
+++ src/ast/visitors/translated-strings.js
@@ -0,0 +1,109 @@
+// @ts-check
+const {BaseVisitor} = require('./base');
+
+/**
+ * @typedef {import('../types.js').Visitor} Visitor
+ * @typedef {import('../types.js').Node} Node
+ * @typedef {import('../types.js').MustacheStatement} MustacheStatement
+ * @typedef {import('../types.js').SubExpression} SubExpression
+ * @typedef {import('../types.js').PathExpression} PathExpression
+ * @typedef {import('../types.js').StringLiteral} StringLiteral
+ * @typedef {import('../types.js').SourceLocation} SourceLocation
+ */
+
+/**
+ * @typedef {Object} TranslatedString
+ * @property {Set<string>} parameters
+ * @property {(SourceLocation & {parameters: string[]})[]} locations
+ */
+
+/**
+ * @param {Node} node
+ * @returns {node is PathExpression}
+ */
+function isPathExpressionNode(node) {
+  return node.type === 'PathExpression';
+}
+
+/**
+ * @param {Node} node
+ * @returns {node is StringLiteral}
+ */
+function isStringLiteralNode(node) {
+  return node.type === 'StringLiteral';
+}
+
+/**
+ * @returns {never}
+ */
+function notImplemented() {
+  debugger;
+  throw new Error('Not implemented');
+}
+
+class TranslatedStringsVisitor extends BaseVisitor {
+  static createContext () {
+    return {
+      translatedStrings: new Map(),
+    };
+  }
+
+  /**
+   * @param {Object} options
+   * @param {{translatedStrings: Map<string, TranslatedString>}} context
+   */
+  constructor(options, context) {
+    super(options, context);
+    this.sourceLines = options.source.split('\n');
+    this.translatedStrings = context.translatedStrings;
+    this.seenLocs = new WeakSet();
+  }
+
+  MustacheStatement = this.consumeTHelper.bind(this);
+  SubExpression = this.consumeTHelper.bind(this);
+
+
+  /**
+   * @param {MustacheStatement | SubExpression} node
+   */
+  consumeTHelper(node) {
+    if (this.seenLocs.has(node.loc)) {
+      return;
+    }
+
+    this.seenLocs.add(node.loc);
+
+    const path = node.path;
+    const blockName = isPathExpressionNode(path) ? path.original : notImplemented();
+    if (blockName !== 't') {
+      return;
+    }
+
+    if (node.params.length !== 1) {
+      notImplemented();
+    }
+
+    const parameter = node.params[0];
+    const translationKey = isStringLiteralNode(parameter) ? parameter.original : notImplemented();
+
+    const store = this.translatedStrings.get(translationKey) ?? {
+      parameters: new Set(),
+      locations: [],
+    };
+
+    /** @type {string[]} */
+    const parameters = [];
+
+    store.locations.push({parameters, ...node.loc});
+    this.translatedStrings.set(translationKey, store);
+
+    if (node.hash) {
+      for (const param of node.hash.pairs) {
+        parameters.push(param.key);
+        store.parameters.add(param.key);
+      }
+    }
+  }
+}
+
+module.exports.TranslatedStringsVisitor = TranslatedStringsVisitor;
